3d8d18540114c2fd114a352dab65c9c5d6e08a61
HalynaLastivka <lastivkagalina@gmail.com>
2023-09-19 01:03:56 +0300

    finish sure

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,7 +6,7 @@ export const Statistics = ({ title, stats }) => {
       {title && <h2 className="title">{title}</h2>}
       <ul className={css.statlist}>
         {stats.map(stat => (
-          <li className={css.item} key={stat.id}>
+          <li className={css.itemstat} key={stat.id}>
             <span className="label">{stat.label}</span>
             <span className="percentage">{stat.percentage}%</span>
           </li>
